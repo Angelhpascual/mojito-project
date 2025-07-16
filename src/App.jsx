7f71ff06af384cb15f86dc69d48de3983fbd04ce
Angelhpascual <Angelhpascual@gmail.com>
2025-07-16 13:35:05 +0200

    feat: add Art and Contact components with GSAP animations for scrolling effects

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Cocktails from "./components/Cocktails"
 import About from "./components/About"
+import Art from "./components/Art"
+import Contact from "./components/Contact"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
@@ -14,6 +16,8 @@ const App = () => {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
+      <Contact />
     </main>
   )
 }
